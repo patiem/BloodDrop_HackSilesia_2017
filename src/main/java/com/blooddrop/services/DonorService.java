e6ceca9764178d28e31edda6bce069d5a8f2a1f8
Tomasz Bujakowski <bujakowski@gmail.com>
2017-09-09 01:21:41 +0200

    Add Service annotation in DonorService

--- src/main/java/com/blooddrop/services/DonorService.java
+++ src/main/java/com/blooddrop/services/DonorService.java
@@ -3,7 +3,9 @@ package com.blooddrop.services;
 import com.blooddrop.donor.Donor;
 import com.blooddrop.repository.DonorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DonorService {
 
     @Autowired
