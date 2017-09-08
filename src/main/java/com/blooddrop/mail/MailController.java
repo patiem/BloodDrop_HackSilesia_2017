5284c62f90f114d8459eec8779fd73ee0480e7f5
sebastiankurant <sebastian.kt7@gmail.com>
2017-09-09 00:10:35 +0200

    Add mail sending functionality

---
+++ src/main/java/com/blooddrop/mail/MailController.java
@@ -0,0 +1,22 @@
+package com.blooddrop.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.context.Context;
+
+@RestController
+public class MailController {
+
+    @Autowired
+    EmailHtmlSender emailHtmlSender;
+
+//    @RequestMapping(value = "/send")
+//    public void send(){
+//        Context context = new Context();
+//        context.setVariable("title", "Lorem Ipsum");
+//        context.setVariable("description", "Lorem Lorem Lorem");
+//        EmailStatus emailStatus = emailHtmlSender.send("sebastian.kt7@gmail.com", "Title of email", "email/notification", context);
+//
+//    }
+}
