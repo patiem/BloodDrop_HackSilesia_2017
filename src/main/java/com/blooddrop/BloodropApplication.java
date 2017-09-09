658c8b4484aee5fd9e19febfbc453c1b98e9c40f
patiem <pp.mikulska@gmail.com>
2017-09-09 03:32:03 +0200

    Merge branch 'main-page' of https://github.com/patiem/BloodDrop_HackSilesia_2017 into main-page
    # Conflicts:
    #	src/main/resources/templates/fill.html

--- src/main/java/com/blooddrop/BloodropApplication.java
+++ src/main/java/com/blooddrop/BloodropApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class  BloodropApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(BloodropApplication.class, args);
 	}
+
+
+
 }
