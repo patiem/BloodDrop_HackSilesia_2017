3ab522aa4b502fb135913b0b519340ec0a7b213a
Piotr <pgurdek@gmail.com>
2017-09-09 03:03:35 +0200

    Added default data info

---
+++ src/main/java/com/blooddrop/bootstrap/DevBootstrap.java
@@ -0,0 +1,33 @@
+package com.blooddrop.bootstrap;
+
+import com.blooddrop.donor.Donor;
+import com.blooddrop.enums.BloodGroup;
+import com.blooddrop.services.BloodRequestService;
+import com.blooddrop.services.DonorService;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    BloodRequestService bloodRequestService;
+    DonorService donorService;
+
+    public DevBootstrap(BloodRequestService bloodRequestService, DonorService donorService) {
+        this.bloodRequestService = bloodRequestService;
+        this.donorService = donorService;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        this.onInit();
+    }
+
+    private void onInit() {
+        donorService.addDonor(new Donor("bujakowski@gmail.com", "Krakow", BloodGroup.B_Rh_minus));
+        donorService.addDonor(new Donor("pgurdek@gmail.com", "Krakow", BloodGroup.Zero_Rh_minus));
+        donorService.addDonor(new Donor("sebastian.kurant@gmail.com", "Krakow", BloodGroup.B_Rh_minus));
+    }
+
+}
